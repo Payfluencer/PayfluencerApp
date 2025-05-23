d59bdabb755bd409045e5dd43d58fe6fb903771b
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-05-23 13:52:20 +0300

    completed auth page and user dashbord navbar

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import UserDashboard from "@/components/UserDashboard";
+
+function Dashboard() {
+  return (
+    <div className="max-w-[1240px] my-0 mx-auto">
+      <UserDashboard />
+    </div>
+  );
+}
+
+export default Dashboard;
